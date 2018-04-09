55d845a953046f3a5d2ef0570100402dcbef46ef
Nehal Udyavar <nehaludyavar@gmail.com>
2018-04-08 18:53:37 -0700

    Manually create an Add button to create contacts (initial test)

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -23,7 +23,7 @@ class ListContacts extends Component {
 
   render() {
     const { query } = this.state
-    const { contacts, onDeleteContact } = this.props
+    const { contacts, onDeleteContact, onNavigate } = this.props
 
     const showingContacts = query === ''
       ? contacts
@@ -41,6 +41,11 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
+          <a
+            className='add-contact'
+            href='#create'
+            onClick={onNavigate}
+            >Add Contact</a>
         </div>
 
         {showingContacts.length !== contacts.length && (
