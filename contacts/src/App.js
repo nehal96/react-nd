5c20b7ce80ade16461f03321596e1a4839bceb39
Nehal Udyavar <nehaludyavar@gmail.com>
2018-04-09 19:48:26 -0700

    Added Route to change webpage content based on URL

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
@@ -35,15 +36,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.screen === 'list' && (
+        <Route exact path='/' render={() => (
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact = {this.removeContact}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact />
-        )}
+        )} />
+        <Route path='/create' component={CreateContact} />
       </div>
     )
   }
