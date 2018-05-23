ebfa7c8d1d18b31accd2ba73ea1798e6ebc757a7
Nehal Udyavar <nehaludyavar@gmail.com>
2018-05-22 21:39:56 -0700

    Filtering books into shelves (through variables)

--- p1-book-tracking-app/src/bookshelf.js
+++ p1-book-tracking-app/src/bookshelf.js
@@ -11,20 +11,35 @@ class BookShelf extends Component {
   render() {
     const { books } = this.props
 
+    let currentlyReadingBooks = books.filter(book =>
+      book.shelf === "currentlyReading")
+
+    let wantToReadBooks = books.filter(book =>
+      book.shelf === "wantToRead")
+
+    let readBooks = books.filter(book =>
+      book.shelf === "read")
+
     return (
       <section className='bookshelf-container'>
         <div className='bookshelf'>
           <div className='bookshelf-heading'>
             <h5>Currently Reading</h5>
+            <ListBooks
+              books={currentlyReadingBooks}
+            />
           </div>
           <div className='bookshelf-heading'>
             <h5>Want to Read</h5>
             <ListBooks
-              books={books}
+              books={wantToReadBooks}
             />
           </div>
           <div className='bookshelf-heading'>
             <h5>Read</h5>
+            <ListBooks
+              books={readBooks}
+            />
           </div>
         </div>
       </section>
