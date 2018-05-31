b27970bb6dbc40a4ff9de554015fac813e4dce94
Nehal Udyavar <nehaludyavar@gmail.com>
2018-05-30 22:53:05 -0700

    New card design for books

--- p1-my-reads/src/Book.js
+++ p1-my-reads/src/Book.js
@@ -11,7 +11,7 @@ class Book extends Component {
   changeShelf = (event) => {
     // Gets shelf value from event
     const shelf = event.target.value
-    
+
     this.props.updateShelf(this.props.book, shelf)
   }
 
@@ -22,9 +22,9 @@ class Book extends Component {
     const title = book.title ? book.title : "No title"
 
     return (
-      <div className='book'>
+      <div className='card book-info-card' style={{ width: '128px'}}>
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})`}}></div>
+          <img className="card-img img-fluid" src={thumbnail} alt={ book.title } style={{ width: '128px', height: '200px'}}></img>
           <div className="book-shelf-changer">
             <select onChange={ this.changeShelf } value={ book.shelf }>
               <option value="none" disabled>Move to...</option>
@@ -35,13 +35,16 @@ class Book extends Component {
             </select>
           </div>
         </div>
-        <div className="book-title">{ title }</div>
-        <div className="book-authors">{
-          book.authors && (
-            book.authors.length > 1 ?
-            book.authors.map((author) => (author + ', ')) :
-            book.authors[0]
-        )}</div>
+        <div className="book-info">
+          <div className="book-title">{ title }</div>
+          <div className="book-authors">{
+            book.authors && (
+              book.authors.length > 1 ?
+              book.authors.map((author) => (author + ', ')) :
+              book.authors[0]
+          )}
+          </div>
+        </div>
       </div>
     )
   }
