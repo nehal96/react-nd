13fd77eb85b8cd76013605d303cac404f62ff1a4
Nehal Udyavar <nehaludyavar@gmail.com>
2018-07-03 20:56:36 -0700

    Add React Router to complete app navigation

--- lessons/real-world-redux/mock-twitter/src/components/Tweet.js
+++ lessons/real-world-redux/mock-twitter/src/components/Tweet.js
@@ -5,6 +5,7 @@ import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
 import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
 import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
   handleLike = (e) => {
@@ -22,7 +23,7 @@ class Tweet extends Component {
   toParent = (e, id) => {
     e.preventDefault()
 
-    // todo: redirect to parent tweet
+    this.props.history.push(`/tweet/${id}`)
   }
 
   render() {
@@ -33,11 +34,11 @@ class Tweet extends Component {
     }
 
     const {
-      name, avatar, timestamp, text, hasLiked, likes, replies, parent
+      name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
     } = tweet
 
     return (
-      <div className='tweet'>
+      <Link to={`/tweet/${id}`} className='tweet'>
         <img src={ avatar } alt={ `Avatar of ${name}` } className='avatar'/>
         <div className='tweet-info'>
           <div>
@@ -61,7 +62,7 @@ class Tweet extends Component {
             <span>{ likes !== 0 && likes }</span>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
@@ -78,4 +79,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
