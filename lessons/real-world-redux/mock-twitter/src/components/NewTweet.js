be18e2c9457453066d62f56dc992f37ab6e9dbd6
Nehal Udyavar <nehaludyavar@gmail.com>
2018-07-03 19:12:11 -0700

    Implement tweet adding logic (action+reducer)

--- lessons/real-world-redux/mock-twitter/src/components/NewTweet.js
+++ lessons/real-world-redux/mock-twitter/src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets'
 
 class NewTweet extends Component {
   state = {
@@ -17,10 +19,9 @@ class NewTweet extends Component {
     e.preventDefault()
 
     const { text } = this.state
+    const { dispatch, id } = this.props
 
-    // todo: add tweet to store
-
-    console.log('New Tweet: ', text)
+    dispatch(handleAddTweet(text, id))
 
     this.setState(() => ({
       text: ''
@@ -30,7 +31,7 @@ class NewTweet extends Component {
   render() {
     const { text } = this.state
 
-    // Redirect to / if submitted 
+    // Redirect to / if submitted
 
     const tweetLeft = 280 - text.length
 
@@ -62,4 +63,4 @@ class NewTweet extends Component {
   }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
