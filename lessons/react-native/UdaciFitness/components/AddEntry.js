eda3bfd0fdca4577ada077b4fb20a51a056f51c5
Nehal Udyavar <nehaludyavar@gmail.com>
2018-07-15 03:10:41 -0700

    Create submit button for AddEntry screen

--- lessons/react-native/UdaciFitness/components/AddEntry.js
+++ lessons/react-native/UdaciFitness/components/AddEntry.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import DateHeader from './DateHeader'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
+
+function SubmitBtn({ onPress }) {
+  return(
+    <TouchableOpacity
+      onPress={ onPress }>
+        <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -46,6 +55,27 @@ export default class AddEntry extends Component {
     }))
   }
 
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    // Update Redux
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0
+    }))
+
+    // Navigate to home
+
+    // Save to DB
+
+    // Clear local notification
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
 
@@ -75,6 +105,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={ this.submit }/>
       </View>
     )
   }
