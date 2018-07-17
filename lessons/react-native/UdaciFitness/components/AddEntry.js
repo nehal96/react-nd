e079ea05a070ed65ba329ce5bcbf8132791a87fc
Nehal Udyavar <nehaludyavar@gmail.com>
2018-07-17 15:06:15 -0700

    Create TextButton component + setup already logged notif. page

--- lessons/react-native/UdaciFitness/components/AddEntry.js
+++ lessons/react-native/UdaciFitness/components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import DateHeader from './DateHeader'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
+import TextButton from './TextButton'
+import { Ionicons } from '@expo/vector-icons'
 
 function SubmitBtn({ onPress }) {
   return(
@@ -76,8 +78,31 @@ export default class AddEntry extends Component {
     // Clear local notification
   }
 
+  reset = () => {
+    const key = timeToString()
+
+    // Update Redux
+
+    // Route to Home
+
+    // Update DB
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name='ios-happy-outline'
+            size={ 100 }
+          />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={ this.reset }>Reset</TextButton>
+        </View>
+      )
+    }
 
     return(
       <View>
