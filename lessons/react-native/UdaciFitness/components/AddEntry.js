62301de525b71921592c035b3bf934d7aa01f255
Nehal Udyavar <nehaludyavar@gmail.com>
2018-07-18 18:44:59 -0700

    Create API that adds/removes data from AsyncStorage

--- lessons/react-native/UdaciFitness/components/AddEntry.js
+++ lessons/react-native/UdaciFitness/components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import TextButton from './TextButton'
 import { Ionicons } from '@expo/vector-icons'
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn({ onPress }) {
   return(
@@ -73,7 +74,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to DB
+    submitEntry({ key, entry })
 
     // Clear local notification
   }
@@ -85,7 +86,7 @@ export default class AddEntry extends Component {
 
     // Route to Home
 
-    // Update DB
+    removeEntry(key)
   }
 
   render() {
