84d857e51841d4431dc3cc0c58818f07adf133df
Nehal Udyavar <nehaludyavar@gmail.com>
2018-07-20 02:42:14 -0700

    Implement App Loading

--- lessons/react-native/UdaciFitness/components/History.js
+++ lessons/react-native/UdaciFitness/components/History.js
@@ -8,8 +8,13 @@ import UdaciFitnessCalendar from 'udacifitness-calendar'
 import DateHeader from './DateHeader'
 import MetricCard from './MetricCard'
 import { white } from '../utils/colors'
+import { AppLoading } from 'expo'
 
 class History extends Component {
+  state = {
+    ready: false
+  }
+
   componentDidMount() {
     const { dispatch } = this.props
 
@@ -22,7 +27,9 @@ class History extends Component {
           }))
         }
       })
-      .then(() => this.setState(() => ({ ready: true })))
+      .then(() => this.setState(() => ({
+        ready: true
+      })))
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
@@ -54,6 +61,11 @@ class History extends Component {
 
   render() {
     const { entries } = this.props
+    const { ready } = this.state
+
+    if (ready === false) {
+      return <AppLoading />
+    }
 
     return (
       <UdaciFitnessCalendar
