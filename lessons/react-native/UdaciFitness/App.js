385563c81f9350c2cb83e1a85a710bdaec6d051d
Nehal Udyavar <nehaludyavar@gmail.com>
2018-07-22 02:27:51 -0700

    Create Live component to play with native features

--- lessons/react-native/UdaciFitness/App.js
+++ lessons/react-native/UdaciFitness/App.js
@@ -3,6 +3,7 @@ import { View, Platform, StatusBar } from 'react-native'
 import AddEntry from './components/AddEntry'
 import History from './components/History'
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
@@ -32,6 +33,13 @@ const Tabs = createBottomTabNavigator({
     navigationOptions: {
       tabBarLabel: 'Add Entry',
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={ 30 } color={ tintColor } />
+    }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={ 30 } color={ tintColor } />
     }
   }
 }, {
