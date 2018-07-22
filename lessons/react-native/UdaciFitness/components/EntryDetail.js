fccb696404d49cd0304187b08f7bd4f4c44da2c4
Nehal Udyavar <nehaludyavar@gmail.com>
2018-07-22 00:11:00 -0700

    Add metrics reset functionality

--- lessons/react-native/UdaciFitness/components/EntryDetail.js
+++ lessons/react-native/UdaciFitness/components/EntryDetail.js
@@ -3,6 +3,10 @@ import { View, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import { white } from '../utils/colors'
 import MetricCard from './MetricCard'
+import TextButton from './TextButton'
+import { addEntry } from '../actions'
+import { removeEntry } from '../utils/api'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
 
 class EntryDetail extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -16,13 +20,28 @@ class EntryDetail extends Component {
       title: `${day}/${month}/${year}`
     }
   }
+
+  reset = () => {
+    const { remove, goBack, entryId } = this.props
+
+    remove()
+    goBack()
+    removeEntry()
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metrics !== null && !nextProps.metrics.today
+  }
+
   render() {
     const { metrics } = this.props
 
     return (
       <View>
         <MetricCard metrics={ metrics } />
-        <Text>Entry Detail - { this.props.navigation.state.params.entryId }</Text>
+        <TextButton onPress={ this.reset } style={{ margin: 20 }}>
+          RESET
+        </TextButton>
       </View>
     )
   }
@@ -36,6 +55,19 @@ const styles = StyleSheet.create({
   }
 })
 
+function mapDispatchToProps(dispatch, { navigation }) {
+  const { entryId } = navigation.state.params
+
+  return {
+    remove: () => dispatch(addEntry({
+      [entryId]: timeToString() === entryId
+        ? getDailyReminderValue()
+        : null
+    })),
+    goBack: () => navigation.goBack()
+  }
+}
+
 function mapStateToProps(state, { navigation }) {
   const { entryId } = navigation.state.params
 
@@ -45,4 +77,4 @@ function mapStateToProps(state, { navigation }) {
   }
 }
 
-export default connect(mapStateToProps)(EntryDetail)
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail)
